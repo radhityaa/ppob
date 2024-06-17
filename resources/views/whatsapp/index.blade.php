b06ccffab8c83a2625814ebc2952999f60a75b0c
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-06-17 23:48:11 +0700

    update

--- resources/views/whatsapp/index.blade.php
+++ resources/views/whatsapp/index.blade.php
@@ -14,31 +14,33 @@
                 </div>
             </div>
         </div>
-        <div class="card-body">
-            <table class="table">
-                <thead>
-                    <th>Number</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ $device->phone }}</td>
-                        <td> <span
-                                class="badge {{ $device->status == 'Connected' ? 'bg-success' : 'bg-danger' }}">{{ $device->status == 'Connected' ? 'Connected' : 'Disconnected' }}</span>
-                        </td>
-                        <td>
-                            @if ($device->status == 'Disconnected')
-                                <a href="{{ route('whatsapp.scan', $device->phone) }}" class="btn btn-success btn-sm"><i
-                                        class="ti ti-qrcode me-2"></i>Scan</a>
-                            @else
-                                <a href="{{ route('whatsapp.scan', $device->phone) }}" class="btn btn-danger btn-sm"><i
-                                        class="ti ti-logout me-2"></i>Logout</a>
-                            @endif
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="card-body p-0">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <th>Number</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $device->phone }}</td>
+                            <td> <span
+                                    class="badge {{ $device->status == 'Connected' ? 'bg-success' : 'bg-danger' }}">{{ $device->status == 'Connected' ? 'Connected' : 'Disconnected' }}</span>
+                            </td>
+                            <td>
+                                @if ($device->status == 'Disconnected')
+                                    <a href="{{ route('whatsapp.scan', $device->phone) }}" class="btn btn-success btn-sm"><i
+                                            class="ti ti-qrcode me-2"></i>Scan</a>
+                                @else
+                                    <a href="{{ route('whatsapp.scan', $device->phone) }}" class="btn btn-danger btn-sm"><i
+                                            class="ti ti-logout me-2"></i>Logout</a>
+                                @endif
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection

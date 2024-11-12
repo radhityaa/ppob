222434e9b63bf43f1c20a1f044f0a926cad66e6a
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-11-12 13:25:20 +0700

    update

--- whatsapp/src/main.js
+++ whatsapp/src/main.js
@@ -8,9 +8,7 @@ const PORT = 4000
 
 const io = new Server(5000, {
     cors: {
-        origin: ["http://127.0.0.1:8000", "http://localhost:8000"],
-        methods: "*",
-        credentials: true
+        origin: "*"
     }
 })
 
